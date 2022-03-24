9133816aee1d3a2f2e8f49481d8a8742da06ada5
Geo-Lu-Zhu <88509927+Geo-Lu-Zhu@users.noreply.github.com>
2022-03-24 02:56:39 -0500

    Add files via upload

---
+++ Service/TwitchException.java
@@ -0,0 +1,7 @@
+package com.laioffer.onlineorder.Service;
+
+public class TwitchException extends RuntimeException{
+    public TwitchException(String errorMessage){
+        super(errorMessage);
+    }
+}
